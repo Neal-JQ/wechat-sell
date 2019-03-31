a71b09bbc87b86c5e01759b5628626e2b2d4ef91
neal_jq <984870240@qq.com>
2019-03-31 22:46:31 +0800

    Add Buyer API

---
+++ src/main/java/com/neal/sell/controller/BuyerProductController.java
@@ -0,0 +1,57 @@
+package com.neal.sell.controller;
+
+import com.neal.sell.entity.ProductCategory;
+import com.neal.sell.entity.ProductInfo;
+import com.neal.sell.service.ProductCategoryService;
+import com.neal.sell.service.ProductInfoService;
+import com.neal.sell.utils.ResultVoUtil;
+import com.neal.sell.vo.ProductInfoVO;
+import com.neal.sell.vo.ProductVO;
+import com.neal.sell.vo.ResultVO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/buyer/product/")
+public class BuyerProductController {
+
+    @Autowired
+    private ProductInfoService productInfoService;
+
+    @Autowired
+    private ProductCategoryService productCategoryService;
+
+    @GetMapping("/list")
+    public ResultVO list(){
+        List<ProductInfo> productInfoList = productInfoService.findUpAll();
+
+        List<Integer> productCategoryTypeList = productInfoList.stream().map(ProductInfo::getCategoryType).collect(Collectors.toList());
+        List<ProductCategory> productCategoryList = productCategoryService.findByCategoryTypeIn(productCategoryTypeList);
+
+        List<ProductVO> productVOs = new ArrayList<>();
+        for(ProductCategory productCategory : productCategoryList){
+            ProductVO productVo = new ProductVO();
+            productVo.setCategoryName(productCategory.getCategoryName());
+            productVo.setCategoryType(productCategory.getCategoryType());
+
+            List<ProductInfoVO> productInfoVOs = new ArrayList<>();
+            for(ProductInfo productInfo: productInfoList){
+                if(productInfo.getCategoryType().equals(productCategory.getCategoryType())){
+                    ProductInfoVO productInfoVo = new ProductInfoVO();
+                    BeanUtils.copyProperties(productInfo, productInfoVo);
+                    productInfoVOs.add(productInfoVo);
+                }
+            }
+            productVo.setProductInfoVOList(productInfoVOs);
+            productVOs.add(productVo);
+        }
+        return ResultVoUtil.success(productVOs);
+    }
+}
---
+++ src/test/java/com/neal/sell/repository/ProductInfoRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.neal.sell.repository;
+
+import com.neal.sell.entity.ProductInfo;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductInfoRepositoryTest {
+
+    @Autowired
+    ProductInfoRepository productInfoRepository;
+
+    @Test
+    public void findByProductStatus() {
+    }
+
+    @Test
+    public void save(){
+        ProductInfo productInfo = new ProductInfo();
+        productInfo.setProductId("123456");
+        productInfo.setProductName("皮蛋粥");
+        productInfo.setProductPrice(new BigDecimal(3.2));
+        productInfo.setProductStock(100);
+        productInfo.setProductDescription("很好喝的粥");
+        productInfo.setProductIcon("http://xxxxx.jpg");
+        productInfo.setProductStatus(0);
+        productInfo.setCategoryType(1);
+
+        ProductInfo result = productInfoRepository.save(productInfo);
+        Assert.assertNotNull(result);
+
+    }
+}
